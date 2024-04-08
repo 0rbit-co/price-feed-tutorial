554b7782b4ef2230f3f42ea3755900986efc2ef4
Not-Sarthak <notsarthakshah@gmail.com>
2024-04-08 23:37:26 +0530

    light theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,21 +20,20 @@ function App() {
   }
 
   return (
-    <div className="flex flex-col min-h-screen bg-black">
+    <div className="flex flex-col min-h-screen bg-white">
       <div className="flex flex-col justify-center items-center flex-1">
-        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white mb-8">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-black mb-8">
           Getting Price Feed using{" "}
           <span className="text-yellow-500">0rbit</span> on{" "}
           <span className="text-yellow-500">AO</span>
         </h1>
 
-        <div className="bg-black rounded-lg shadow-lg p-8 w-full max-w-md lg:max-w-lg border-white/40 border-2">
+        <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md lg:max-w-lg border-white/40 border-2">
           <div className="mb-6">
             <ConnectButton
-              accent="white"
+              // accent="white"
               profileModal={true}
               showBalance={false}
-              className="text-black"
             >
               Connect Wallet
             </ConnectButton>
@@ -42,14 +41,14 @@ function App() {
           <div className="mb-6">
             <button
               onClick={() => setPrice()}
-              className="bg-white hover:bg-zinc-300 text-black font-bold py-3 px-6 rounded-lg focus:outline-none focus:shadow-outline w-full"
+              className="bg-black hover:bg-zinc-900 text-white font-bold py-3 px-6 rounded-lg focus:outline-none focus:shadow-outline w-full"
               disabled={loading}
             >
               {loading ? "Fetching Price..." : "Get Price"}
             </button>
           </div>
           {currentPrice && (
-            <div className="bg-[#0F0F0F] text-white rounded-lg shadow-lg p-4 md:p-6 lg:p-8">
+            <div className="bg-white text-black rounded-lg shadow-lg p-4 md:p-6 lg:p-8">
               <h2 className="text-xl md:text-2xl font-bold mb-4">
                 Current Bitcoin Price
               </h2>
@@ -61,9 +60,9 @@ function App() {
           )}
         </div>
         <div className="pt-8"></div>
-        <div className="flex flex-col justify-start border-white/40 border-2 p-8 bg-[#0F0F0F] rounded-lg">
-          <h2 className="text-white pt-10">Useful Links:</h2>
-          <div className="text-white grid grid-cols-2 gap-4 pt-4">
+        <div className="flex flex-col justify-start border p-8 bg-white rounded-lg shadow-lg">
+          <h2 className="text-black">Useful Links:</h2>
+          <div className="text-black grid grid-cols-2 gap-4 pt-4">
             <a href="https://docs.0rbit.co/" className="text-yellow-500">
               Documentation
             </a>
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 const Navbar = () => {
   return (
-    <nav className="bg-black py-4 px-6 md:px-12 lg:px-16">
+    <nav className="bg-white py-4 px-6 md:px-12 lg:px-16">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         <div className="flex items-center">
-          <img className="h-8 md:h-10" src="../public/favicon.svg" alt="Logo" />
+          <img className="h-8 md:h-10" src="../src/assets/favicon.svg" alt="Logo" />
         </div>
       </div>
     </nav>
